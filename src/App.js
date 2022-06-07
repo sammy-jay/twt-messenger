f3d3164dd64d53b7fc0cfe372c881da8405a8ee9
sammy-jay <sammyjayden27@gmail.com>
2022-06-07 17:54:47 +0000

    testing actions

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="bg-gray-200 w-full ">
       <h1 className="text-3xl text-blue-700">hello dev</h1>
-      <p> testing github actions<p>
+      <p> testing github actions</p>
     </div>
   );
   
